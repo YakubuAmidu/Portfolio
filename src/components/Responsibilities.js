13f2630fe5353b76eb350a27517b7260d2d3b20b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-07 23:37:40 -0800

    Commiting changes

--- src/components/Responsibilities.js
+++ src/components/Responsibilities.js
@@ -4,44 +4,29 @@ import { Col, Row } from "react-bootstrap";
 class Responsibilities extends Component {
   render() {
     return (
-      <Row>
-        <Col md={12} className="Responsibilities">
-          <i class="fab fa-react"></i>
-          <p className="Responsibilities_header">ReactJS</p>
-          <p className="Responsibilities_paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-        </Col>
-        <Col md={12} className="Responsibilities">
-          <i class="fab fa-react"></i>
-          <p className="Responsibilities_header">React native</p>
-          <p className="Responsibilities_paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-        </Col>
-        <Col md={12} className="Responsibilities">
-          <i class="fab fa-js"></i>
-          <p className="Responsibilities_header">Javascript</p>
-          <p className="Responsibilities_paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-        </Col>
-        <Col md={12} className="Responsibilities">
-          <i class="fab fa-bootstrap"></i>
-          <p className="Responsibilities_header">Bootstrap</p>
-          <p className="Responsibilities_paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis
-          </p>
-        </Col>
-      </Row>
+      <div>
+        <h1>Skills</h1>
+        <Row>
+          <Col md={12} className="Responsibilities">
+            <i class="fab fa-react"></i> ReactJS
+          </Col>
+          <Col md={12} className="Responsibilities">
+            <div className="Skills_logo">
+              <i class="fab fa-react"></i> React native
+            </div>
+          </Col>
+          <Col md={12} className="Responsibilities">
+            <div>
+              <i class="fab fa-js"></i> Javascript
+            </div>
+          </Col>
+          <Col md={12} className="Responsibilities">
+            <div>
+              <i class="fab fa-bootstrap"></i> Bootstrap
+            </div>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
