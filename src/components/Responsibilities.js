cbfbea4a47e1029ff476790bf37dcd514eb9b9d2
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-07 21:42:59 -0800

    Commiting changes

---
+++ src/components/Responsibilities.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Responsibilities extends Component {
+  render() {
+    return (
+      <div>
+        <i class="fas fa-laptop-code"></i>
+        <p>Write something here</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Responsibilities;
